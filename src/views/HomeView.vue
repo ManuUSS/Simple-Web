752856f3c8cc5a00b8a0c20b4184b3d241f33859
Manuel Ulate <fr3zs09@gmail.com>
2022-12-13 21:04:56 -0600

    efecto paralax

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="home">
     <section class="home__section">
-      <img class="home__img-mountains" src="../assets/mountains.png"/>
+      <img class="home__img-background" src="../assets/bg.png"/> 
+      <img class="home__img-mountains3" src="../assets/mt3.png"/>
+      <img class="home__img-mountains" src="../assets/mt1.png"/> 
+      <img class="home__img-mountains2" src="../assets/mt2.png"/>
       <img class="home__img-grass" src="../assets/grass.png"/>
       <h1>Welcome!</h1>
     </section>
@@ -24,7 +27,7 @@ export default {
   height: 100vh;
   overflow-x: hidden;
   overflow-y: auto;
-  perspective: 0.625rem;
+  perspective: 0.9375rem;
 
   &__section {
     align-items: center;
@@ -34,20 +37,66 @@ export default {
     position: relative;
     transform-style: preserve-3d;
     z-index: -1;
+    //overflow: hidden;
   }
 
-  &__img-mountains {
-    height: 100%;
+  &__img-background {
     object-fit: cover;
     object-position: center;
+    top: 0;
+    position: absolute;
+    width: 100%;
+    z-index: -2;
+    transform: translateZ(-16px) scale(2.1);
+  }
+
+  &__img-mountains3 {
+    object-fit: cover;
+    object-position: center;
+    top: 0.75rem;
     position: absolute;
     width: 100%;
     z-index: -1;
+    transform: translateZ(-10px) scale(1.8);
   }
 
-  &__img-grass {
+
+  &__img-mountains {
+    object-fit: cover;
+    object-position: center;
+    bottom: -7.5rem;
     position: absolute;
+    width: 100%;
+    z-index: -1;
+    transform: translateZ(-7px) scale(1.7);
+  }
+
+  &__img-mountains2 {
+    object-fit: cover;
+    object-position: center;
+    left: 0;
+    bottom: -0.625rem;
+    position: absolute;
+    width: 28%;
+    z-index: -1;
+    transform: translateZ(-3px) scale(1.8);
+  }
+
+
+  &__img-grass {
     bottom: 0;
+    position: absolute;
+    width: 78%;
+    z-index: -1;
+    transform: translateZ(-2px) scale(1.6);
+  }
+
+  h1 {
+    color: #fff;
+    font-size: 6.875rem;
+    font-weight: 700;
+    letter-spacing: 0.1rem;
+    user-select: none;
   }
     
 }
